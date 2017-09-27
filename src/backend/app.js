80ac6e19811754d52eb806b40030fd69fd559921
Amit binu <binua@mcmaster.ca>
2017-09-27 14:17:27 -0400

    Added data url

---
+++ src/backend/app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var url = require('url');
+var request = require('request');
+
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+var realData = null;
+
+request('https://www.timetablegenerator.io/api/v2/school/mcmaster', function (error, response, body) {
+  if (error){
+    console.log('Something went wrong');
+  }
+  else{
+    console.log('Got the data');
+    realData = JSON.parse(body);
+    console.log(realData.name);
+    app.locals.dataSet  =realData;
+  }
+})
+
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
