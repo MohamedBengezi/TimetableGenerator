5cd8b8cba02b7d715ef364033fc2d693f19fc495
Amit binu <binua@mcmaster.ca>
2017-09-28 17:04:19 -0400

    Project files

---
+++ src/backend/public/javascripts/main.js
@@ -0,0 +1,40 @@
+
+$('#output').innerHTML = "<p>Hello</p>";
+$(document).ready(function () {
+    var allCourses = [];
+    $('#add').click(function () {
+        var value = $('#input').val();
+        if (value === ''){
+            $('#error').html("Please enter a course code");
+            $('#error').effect('shake');
+        }
+        else{
+            $('#error').html(" ");
+            $.ajax({
+                url: '/check/' + value,
+                type:"POST"
+            }).done(function (result) {
+                if (result === "added"){
+                    $("#error").html("You already chose this course");
+                    return;
+                }
+                if (result === "error"){
+                    $("#error").html("Invalid course");
+                    $('#error').effect('shake');
+                    return;
+                }
+                console.log('done ' + result);
+                $('#output').html(result);
+            })
+        }
+
+    });
+    $.ajax({
+        url:'/getlink',
+        type:"POST"
+    }).done(function (result) {
+        console.log(result);
+        $("#input").autocomplete({ maxShowItems:10,source:result, minLength:4, delay:0});
+    })
+    $.getJSON("https")
+})
