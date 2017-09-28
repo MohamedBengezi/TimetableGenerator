5dc7b2faed0ad1cd14c6d1ed29db8383fee87177
Amit binu <binua@mcmaster.ca>
2017-09-28 17:08:36 -0400

    Modified to receive a request to send array of course ids

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log("Searching for "+ req.params);
   res.render('index', { title: 'Express' });
+
 });
+
+router.post('/getlink',function (req,res,next) {
+    var courses = require('../app').macCourses;
+    console.log(req.params);
+    res.send(courses);
+    res.end();
+})
 
 module.exports = router;
