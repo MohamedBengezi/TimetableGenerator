dabd7a300845fc5c84cf81b3eb53ce24dd91e972
Amit binu <binua@mcmaster.ca>
2017-10-05 09:53:57 -0400

    Updated to reset arrays when page is reloaded

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var reload = require('./checkCourse').reloadReset;
+var schedulerReload = require('./scheduler').reset;
 /* GET home page. */
 router.get('/', function(req, res, next) {
     reload();
-
+    schedulerReload();
     res.render('index', { title: 'Express' });
     res.end();
 });
