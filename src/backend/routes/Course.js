c5b78e6a4ce129b9c56b3c8fc98bb6f8af92bd6d
Amit binu <binua@mcmaster.ca>
2017-10-05 09:54:40 -0400

    A course object that represents a course

---
+++ src/backend/routes/Course.js
@@ -0,0 +1,43 @@
+
+
+var course = function (name,lectureTimes,tutorialTimes,labTimes,term) {
+    this.name = name;
+    this.lectureTimes = lectureTimes;
+    this.tutorialTimes = tutorialTimes;
+    this.labTimes = labTimes;
+    this.term = term;
+}
+
+/*
+    lectureTimes
+        - Each element is an array that contains information about one core.
+        - For example, lectureTimes[0] is an array of TimeObjects about C01 of a course.
+
+        TimeObject
+            - It is an object with following attributes.
+            - day
+            - start
+            - end
+            - core
+            - room
+            - supervisor
+
+
+     labTimes
+        - Each element is an array that contains information about one lab.
+        - For example, labTimes[0] is an array of TimeObjects about L01 of a course.
+
+        TimeObject
+            - It is an object with following attributes.
+            - day
+            - start
+            - end
+            - lab
+            - room
+            - supervisor
+
+ */
+
+// NOTE If a course is being offered in both semesters, then it will be at 2 indicies in CourseIds in app.js / macCourses (exported variable).
+
+module.exports.macCourse = course;
