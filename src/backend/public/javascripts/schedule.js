99577d0a6a186b2d835dae3993e20715de86c398
Amit binu <binua@mcmaster.ca>
2017-10-18 18:59:15 -0400

    Updated schedule.js

--- src/backend/public/javascripts/schedule.js
+++ src/backend/public/javascripts/schedule.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
     }).done(function (result) {
         putCourses(result)
     });
-
+    var colors=[];
+    var courses=[];
+    function randomColor() {
+        var back = ["#a0c4ff","#ffa0ff","#ffa0a0","#c6c4c4","#fff8ba","#c0ffba","#bafff8","#ffbae9","#4ef499","#b657f2"];
+        return back[Math.floor(Math.random() * back.length)];
+    }
     function putCourses(schedule) {
         var counter = 1;
         console.log(schedule);
@@ -20,22 +25,40 @@ $(document).ready(function () {
 
                     var day = core.day-1;
 
-                    if(day===5){
-                        day= 4;
-                    }
+
                     var name = core.name;
-                    if(name == "MATH-2Z03 T03"){
-                        console.log(day);
+                    var code = core.name.split(' ');
+                    code= code[0];
+                    var color;
+                    if (courses.indexOf(code) < 0){
+                        console.log('making a color for ' + code);
+                        //make a randomColor;
+
+                        color = randomColor();
+                        while(colors.indexOf(color) >=0 ){
+                            color = randomColor();
+                        }
+                        colors.push(color);
+                        courses.push(code);
+                    }
+                    else{
+                        var index = courses.indexOf(code);
+                        color= colors[index];
                     }
                     var cell = $("#schedule"+counter+' .time'+startTime + " .day" +day);
-                    var cell2 = $("#schedule"+counter+' .time'+endTime + " .day" +day);
+
                     console.log(rowspan);
+
+                    for(var start= core.start; start <= endTime+0.5; start += 0.5){
+                        $("#schedule"+counter+' .time'+start + " .day" +day).remove();
+                    }
                     cell.attr('rowspan',++rowspan);
-                    cell2.attr('rowspan',++rowspan)
                    cell.html(name);
+                   cell.css('background',color);
                 })
             });
             counter++;
-        })
+        });
+     //   $("#schedule"+1+' .time'+15 + " .day" +0).remove();
     }
 });
