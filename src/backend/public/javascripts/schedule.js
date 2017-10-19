ced35cfe47a352d40fc216576d00fc11d93e3160
Amit binu <binua@mcmaster.ca>
2017-10-19 02:05:05 -0400

    Added room and supervisor infos

--- src/backend/public/javascripts/schedule.js
+++ src/backend/public/javascripts/schedule.js
@@ -9,20 +9,18 @@ $(document).ready(function () {
     var colors=[];
     var courses=[];
     function randomColor() {
-        var back = ["#a0c4ff","#ffa0ff","#ffa0a0","#c6c4c4","#fff8ba","#c0ffba","#bafff8","#ffbae9","#4ef499","#b657f2"];
+        var back = ["#a0c4ff","#ffa0ff","#ffa0a0","#c6c4c4","#fff8ba","#a7f99f","#bafff8","#bebaff","#ffca96"];
         return back[Math.floor(Math.random() * back.length)];
     }
     function putCourses(schedule) {
         var counter = 1;
-        console.log(schedule);
         schedule.forEach(function (version) {
             version.forEach(function (day) {
                 day.forEach(function (core) {
                     var startTime = core.start+'';
                     startTime = startTime.replace('.','');
                     var endTime= core.end;
-                    var rowspan = Math.round((core.end-core.start)/0.5);
-
+                    var rowspan = Math.round((core.end-core.start+0.3)/0.5);
                     var day = core.day-1;
 
 
@@ -31,7 +29,6 @@ $(document).ready(function () {
                     code= code[0];
                     var color;
                     if (courses.indexOf(code) < 0){
-                        console.log('making a color for ' + code);
                         //make a randomColor;
 
                         color = randomColor();
@@ -47,13 +44,14 @@ $(document).ready(function () {
                     }
                     var cell = $("#schedule"+counter+' .time'+startTime + " .day" +day);
 
-                    console.log(rowspan);
 
-                    for(var start= core.start; start <= endTime+0.5; start += 0.5){
-                        $("#schedule"+counter+' .time'+start + " .day" +day).remove();
+                    for(var start= core.start+0.5; start <= endTime; start += 0.5){
+                        var temp = start+'';
+                        temp = temp.replace('.','');
+                        $("#schedule"+counter+' .time'+temp + " .day" +day).remove();
                     }
-                    cell.attr('rowspan',++rowspan);
-                   cell.html(name);
+                    cell.attr('rowspan',rowspan);
+                   cell.html(name+'<br>'+core.room+'<br>'+core.supervisor);
                    cell.css('background',color);
                 })
             });
